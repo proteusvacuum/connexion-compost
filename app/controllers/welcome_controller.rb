e84eefaa291d162146771dd31709fb76f28dbfd0
Farid Rener <proteusvacuum@gmail.com>
2013-10-19 14:08:04 -0400

    no users in databse fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 		@user = current_user
-		if @user.id? 
-			@composters = Composter.where({user_id: @user.id})
-		end
+		# @composters = Composter.where({user_id: @user.id})
 	end
 end
