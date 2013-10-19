927aad03e8901b01ef602111f6159619735befb4
Farid Rener <proteusvacuum@gmail.com>
2013-10-19 19:11:29 -0400

    now it's time to present. Murphy's law, here's to you

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
 	def index
 		# @user = current_user
+		@json = Composter.all.to_gmaps4rails
 		# @composters = Composter.where({user_id: @user.id})
 		@composters = Composter.all
-		@json = Composter.all.to_gmaps4rails
+		
 	end
 end
